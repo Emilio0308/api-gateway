af8ca941c10649b42a85aa8f48955c3a321259b5
emilio rivas <emiliorivasruiz@gmail.com>
2023-11-07 23:46:07 -0500

    fetch is not defined xD

--- src/utils/startServices.js
+++ src/utils/startServices.js
@@ -1,10 +1,17 @@
+const axios = require('axios');
+
 exports.startservices = () => {
-    const services = [
-      'https://user-service-aav2.onrender.com',
-      'https://email-service-x8gr.onrender.com/',
-      'https://chat-service-qa46.onrender.com',
-      'https://call-service-uxn7.onrender.com'
-    ]
-  
-    services.forEach(url => fetch(url))
-  }
+  const services = [
+    'https://user-service-aav2.onrender.com',
+    'https://email-service-x8gr.onrender.com/',
+    'https://chat-service-qa46.onrender.com',
+    'https://call-service-uxn7.onrender.com',
+  ];
+
+  services.forEach((url) => {
+    axios
+      .get(url)
+      .then((res) => res.data)
+      .catch((err) => err.message);
+  });
+};
